35d64f4da0f8f9d3dbe82f3e15345e4bf45a986e
Peter Durham <peterdurham@protonmail.com>
2019-02-06 05:09:54 -0800

    readme update

--- src/App.js
+++ src/App.js
@@ -1,83 +1,85 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
+import "./App.css";
 
-import Modal from './Components/Modal';
-import Search from './Components/Search';
-import Results from './Components/Results';
+import Modal from "./Components/Modal";
+import Search from "./Components/Search";
+import Results from "./Components/Results";
 
 class App extends Component {
   state = {
     searchResults: [],
-    searchInput: '',
-    lastInput: '',
+    searchInput: "",
+    lastInput: "",
     viewingImage: false,
     imageToView: null
-  }
-  
-  bindSearchInputHandler = (input) => {
-    this.setState(()=>({ searchInput: input }))
-  }
-  searchImagesHandler = (input) => {
+  };
+
+  bindSearchInputHandler = input => {
+    this.setState(() => ({ searchInput: input }));
+  };
+  searchImagesHandler = input => {
     input = input.replace(" ", "+");
     const API_KEY = process.env.REACT_APP_API_KEY;
-    
-    let URL = `https://pixabay.com/api/?key=${API_KEY}&q=${input}&image_type=photo`
+
+    let URL = `https://pixabay.com/api/?key=${API_KEY}&q=${input}&image_type=photo`;
     return fetch(URL)
-          .then(r => r.json())
-          .then(data => {
-              console.log(data.hits);
-              let searchResults = [];
-              data.hits.forEach((hit)=>{
-                let result = {
-                  id: hit.id,
-                  previewWidth: hit.previewWidth,
-                  previewURL: hit.previewURL,
-                  largeImageURL: hit.largeImageURL,
-                  webformatHeight: hit.webformatHeight,
-                  webformatWidth: hit.webformatWidth,
-                  webformatURL: hit.webformatURL
-                }
-                searchResults.push(result);
-              })
-              this.setState(()=>({ searchResults, lastInput: input }))
-          })
-          .catch(err => {
-              console.log(err)
-          })
-  } 
-  viewImageHandler = (idToView) => {
-    setTimeout(()=>{
+      .then(r => r.json())
+      .then(data => {
+        console.log(data.hits);
+        let searchResults = [];
+        data.hits.forEach(hit => {
+          let result = {
+            id: hit.id,
+            previewWidth: hit.previewWidth,
+            previewURL: hit.previewURL,
+            largeImageURL: hit.largeImageURL,
+            webformatHeight: hit.webformatHeight,
+            webformatWidth: hit.webformatWidth,
+            webformatURL: hit.webformatURL
+          };
+          searchResults.push(result);
+        });
+        this.setState(() => ({ searchResults, lastInput: input }));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  viewImageHandler = idToView => {
+    setTimeout(() => {
       let searchResults = [...this.state.searchResults];
-      let filtered = searchResults.filter((result)=>result.id === idToView);
+      let filtered = searchResults.filter(result => result.id === idToView);
       let imageToView = filtered[0].largeImageURL;
-      ;
-      this.setState(()=>({ imageToView, viewingImage: true }))
-    }, 400)
-    
-  }
+      this.setState(() => ({ imageToView, viewingImage: true }));
+    }, 400);
+  };
   viewCancelHandler = () => {
-    this.setState(()=>({ viewingImage: false }))
-  }
-  
+    this.setState(() => ({ viewingImage: false }));
+  };
+
   render() {
-    
-
-
     return (
       <div className="App">
-        <Modal show={this.state.viewingImage} modalClosed={this.viewCancelHandler}>
-          <img src={this.state.imageToView} className="modal__image" alt="viewed"/>
+        <Modal
+          show={this.state.viewingImage}
+          modalClosed={this.viewCancelHandler}
+        >
+          <img
+            src={this.state.imageToView}
+            className="modal__image"
+            alt="viewed"
+          />
         </Modal>
         <Search
-          searchInput = {this.state.searchInput}
-          searchImages = {this.searchImagesHandler}
-          bindSearchInput = {this.bindSearchInputHandler}
+          searchInput={this.state.searchInput}
+          searchImages={this.searchImagesHandler}
+          bindSearchInput={this.bindSearchInputHandler}
         />
-        <Results 
-          searchResults = {this.state.searchResults}
-          lastInput = {this.state.lastInput}
-          viewImage = {this.viewImageHandler}
+        <Results
+          searchResults={this.state.searchResults}
+          lastInput={this.state.lastInput}
+          viewImage={this.viewImageHandler}
         />
       </div>
     );
